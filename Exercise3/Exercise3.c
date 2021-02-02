f7c6f7e4bebec71b2846d025177d61c099789214
pessiraunio <pessi.raunio@edu.turkuamk.fi>
2021-02-02 21:35:04 +0200

    Random number multiplication added

--- Exercise3/Exercise3.c
+++ Exercise3/Exercise3.c
@@ -4,9 +4,12 @@ Description:	Program for calculating stuff according to Exercise 3
 */
 
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 void sumOfNumbers();
 void sumOfEvenNumbers();
+void multiplyOfNumbers();
 
 int main() {
 	//Calling the function for calculating numbers 1-100.
@@ -14,6 +17,9 @@ int main() {
 	
 	//Calling the function to calculate sum of even numbers between 1 and 1000.
 	sumOfEvenNumbers();
+
+	//Calling the function to do calculations with random numbers between 2-20.
+	multiplyOfNumbers();
 	
 	
 
@@ -60,6 +66,39 @@ void sumOfEvenNumbers() {
 		}
 	}
 	printf("Sum of even numbers 1-1000 is %d\n", sumNumber);
+
+
+
+}
+
+void multiplyOfNumbers() {
+
+	//Needs to be long, otherwise gives invalid integers
+	long calculatedNumber = 1;
+
+	//Printing out the random number.
+	int randomNumber;
+	int lowerLimit = 2, upperLimit = 20;
+	//Getting seed for the random number using seconds passed since 1971
+	srand(time(NULL));
+	randomNumber = (rand() % (lowerLimit-upperLimit+1)) + lowerLimit;
+	printf("\nRandom number is %d\n",randomNumber);
+
+	//Size of and integer is 4 and maximum positive integer is 32767 so maximum integer is 131068
+	//Looping while calculation stays within the limits.
+	do {
+		calculatedNumber = randomNumber * randomNumber;
+		randomNumber = calculatedNumber;
+
+		//Checking if the calculation would end up going over the limit
+		//If no, then printing out calculation
+		if (calculatedNumber * calculatedNumber > (130321)) {
+			printf("Largest calculated number before limit is %ld\n",calculatedNumber);
+		}
+
+	}
+	
+	while (calculatedNumber * calculatedNumber <= (131068) && calculatedNumber != 0);
 
 
 
