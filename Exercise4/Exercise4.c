4256b6a66533fd0d46da692c9c0b3e3608773fa1
pessiraunio <pessi.raunio@edu.turkuamk.fi>
2021-02-09 17:51:26 +0200

    Task 3.b added

--- Exercise4/Exercise4.c
+++ Exercise4/Exercise4.c
@@ -9,6 +9,7 @@ Filename:       Exercise4.c'
 
 //Declaring function to ask for user integers.
 void integerSorter();
+void sumOfInputs();
 
 #include <stdio.h>
 #include <ctype.h>
@@ -19,6 +20,12 @@ int main() {
     
     //Calling function
     integerSorter();
+
+    //Printing this to tidy up the output
+    printf("\n * * * * * * * * *\n");
+
+    //Calling function
+    sumOfInputs();
     
 
     return 0;
@@ -50,8 +57,35 @@ void integerSorter() {
     }
     //Printing out the number of negative integers entered
     printf("\nNumber of negative integers entered: %d\n", negativeNumber);
-    //Printing this to tidy up the output
-    printf("\n * * * * * * * * *\n");
 }
 
+
+
+void sumOfInputs() {
+
+    int userInput = 1;
+    int inputSumCounter = 0;
+
+    //Asking for a input as long as userIput is not 0
+    while (userInput != 0) {
+
+        printf("Enter a number: ");
+        scanf("%d", &userInput);
+
+        //Using for loop to check the number, to avoid infinite loops.
+        for (int number = 1; number >=0; number--) {
+            if (userInput == 0) {
+                break;
+            }
+            else {
+                inputSumCounter = inputSumCounter + userInput;
+                continue;
+        }
+    }
+    }
+    //Printing out the sum of numbers divided by two to get the correct answer.
+    printf("Sum of numbers entered: %d\n", inputSumCounter/2);
+
+
+}
 
