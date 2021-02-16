8876c5089a417a6ff81189ea5d8bec939550e33f
pessiraunio <pessi.raunio@edu.turkuamk.fi>
2021-02-16 20:25:17 +0200

    Exercise 5 Task3.b

--- Exercise5/Exercise5.c
+++ Exercise5/Exercise5.c
@@ -8,6 +8,10 @@ FIlename:       Exercise5.c
 #include <string.h>
 #include <stdlib.h>
 
+//Declaring functions
+int isPrimeNumber(int *numberToCheck);
+void exit(int status);
+
 //Main function with parameters argc and argv
 int main(int argc, char *argv[]) {
 
@@ -26,7 +30,6 @@ int main(int argc, char *argv[]) {
 
     //Using for loop to check that each element is some integer.
     for (int i=0; i<inputLenght; ++i) {
-
         if (stringIndex[i] == '1' || stringIndex[i] == '2' || stringIndex[i] == '3'
             || stringIndex[i] == '4' || stringIndex[i] == '5' || stringIndex[i] == '6'
             || stringIndex[i] == '7' || stringIndex[i] == '8' || stringIndex[i] == '9'
@@ -34,6 +37,7 @@ int main(int argc, char *argv[]) {
             continue;
         }
         else {
+            //If any of the elements is not an integer, user prompt and break the loop.
             printf("Invalid input, only integers allowed\n");
             break;
         }
@@ -46,6 +50,49 @@ int main(int argc, char *argv[]) {
     //Printing out the argument for the user to see.
     printf("\nCommand line argument entered: %s\n", argv[1]);
 
+    printf("\n------------------------------------\n");
+
+    isPrimeNumber(&testInteger);
 
     return 0;
 }
+
+
+int isPrimeNumber(int *numberToCheck) {
+
+    //Variable to indicate if number is prime
+    int noPrime = 1;
+
+    //Checking if the number to check is valid
+    if (*numberToCheck <= 0) {
+        printf("Input must be an integer and it must be greater than 0\n");
+        //Exiting the program if number is invalid
+        exit(0);
+    }
+    //Using for loop to check each element if it is devisible by the prime number
+    for (int i=2; i<*numberToCheck; ++i) {
+        
+        //If number is devisible with something else than itself or 1 noPrime is set to 0 and loop break.
+        if (*numberToCheck % i == 0) {
+            noPrime = 0;
+            break;
+        }
+    }
+    //If number is 1 prompting user
+    if (*numberToCheck == 1) {
+        printf("Number 1 is not prime or composite\n");
+    }
+    //Else giving the input number and information if it is a prime or not
+    else {
+        if (noPrime == 1) {
+            printf("Number %d is a prime number\n", *numberToCheck);
+        }
+        else {
+            printf("Number %d is not a prime number\n", *numberToCheck);
+        }
+    }
+    //Returning 1 if number is prime, and 0 if it is not
+    return printf("function return %d\n", noPrime);
+}
+
+
