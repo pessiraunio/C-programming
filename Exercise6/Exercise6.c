756f58d0dbb272346ccf01f0a035cb7a0f2507a1
pessiraunio <pessi.raunio@edu.turkuamk.fi>
2021-03-01 15:33:57 +0200

    Task3.b added

--- Exercise6/Exercise6.c
+++ Exercise6/Exercise6.c
@@ -13,9 +13,17 @@ Description:    Code for tasks in Exercise 6.
 //Defining array size as a macro just for practise.
 #define ARRAYSIZE 10
 
+//Delay function to stall the time based number generator to get variety on the numbers.
+void delay(int seconds);
+void delay(int seconds) {
+    int milSecond = 1000*seconds;
+    clock_t startTime = clock();
+    while(clock()<startTime+milSecond);
+}
 
 void iterateWithPointers(int *arrayPointer, int arraySize);
 int fillArray(int *emptyArray, int arraySize);
+int arrangeArray(int *unorderedArray, int arraySize);
 
 int main() {
 
@@ -34,6 +42,10 @@ int main() {
     iterateWithPointers(pointerToArray, ARRAYSIZE);
 
     fillArray(pointerToEmptyArray, ARRAYSIZE);
+    iterateWithPointers(pointerToEmptyArray, ARRAYSIZE);
+
+    arrangeArray(pointerToEmptyArray, ARRAYSIZE);
+    iterateWithPointers(pointerToEmptyArray, ARRAYSIZE);
 
     return 0;
 }
@@ -51,6 +63,7 @@ void iterateWithPointers(int *arrayPointer, int arraySize) {
 
 int fillArray(int *emptyArray, int arraySize) {
 
+    //For loop for generating numbers using time based generator.
     for (int i=0; i<arraySize; i++) {
 	    int randomNumber = 0;
 	    int lowerLimit = 0;
@@ -58,9 +71,35 @@ int fillArray(int *emptyArray, int arraySize) {
 
 	    srand(time(NULL));
 	    randomNumber = (rand() % (lowerLimit-upperLimit+1)) + lowerLimit;
+        //Delay added for variety on numbers generated.
+        delay(650);
         emptyArray[i]=randomNumber;
 
     }
-
+    printf("\n---- Array of random generated numbers ----\n\n");
     return emptyArray[0];
+
+}
+
+int arrangeArray(int *unorderedArray, int arraySize) {
+
+    //Temporary number used to iterate array.
+    int tempNumber = 0;
+
+    //Two for loops for checking if number i is smaller than the next number j.
+    for (int i=0; i < arraySize; ++i) {
+
+        for (int j=i+1; j < arraySize; ++j) {
+
+            if (unorderedArray[i] < unorderedArray[j]) {
+                //If number i is smaller temporary number will be used to swap places with number i and j.
+                tempNumber = unorderedArray[i];
+                unorderedArray[i] = unorderedArray[j];
+                unorderedArray[j] = tempNumber;
+            }
+        }
+    }
+    //Returnin the sorted array as numberArray.
+    printf("\n---- Array of random numbers in decreasing order ----\n\n");
+    return unorderedArray[0];
 }
